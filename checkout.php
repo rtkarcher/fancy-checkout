bf975ae525c673cdef52dece007f2d5f2f3553a6
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-10-07 15:23:43 -0500

    Added 'Total Payment', 'Pay Now', 'Cancel' options

--- checkout.php
+++ checkout.php
@@ -25,43 +25,54 @@
 <body>
   <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-md-4">
+	  <div class="col-xs-12 col-md-4">
             <div class="panel-heading">
               <h3 class="panel-title">Payment Details</h3>
             </div> <!-- .panel-heading -->
-                <div class="panel-body">
-                    <form role="form">
-                      <div class="form-group">
-                        <label for="cardNumber">Card Number</label>
-                          <div class="input-group">
-                            <input type="text" class="form-control" id="cardNumber" placeholder="Valid Card Number" required autofocus />
-                            <span class="input-group-addon">
-                              <span class="glyphicon glyphicon-lock"></span>
-                            </span>
-                          </div> <!-- .input-group -->
-                      </div> <!-- .form-group -->
-                    <div class="row">
-                        <div class="col-xs-7 col-md-7">
-                            <div class="form-group">
-                                <label for="expiryMonth">Card Expiry Date</label>
-                                <div class="col-xs-6 col-lg-6 pl-ziro">
-                                    <input type="text" class="form-control" id="expityMonth" placeholder="MM" required />
-                                </div> <!-- .col-xs-6 .col-lg-6 .pl-ziro -->
-                                <div class="col-xs-6 col-lg-6 pl-ziro">
-                                    <input type="text" class="form-control" id="expityYear" placeholder="YY" required />
-                                </div> <!-- .col-xs-6 .col-lg-6 .pl-ziro -->
-                            </div> <!-- .form-group -->
-                        </div> <!-- .col-xs-7 .col-md-7 -->
-                        <div class="col-xs-5 col-md-5 pull-right">
-                            <div class="form-group">
-                                <label for="cvCode">CV Code</label>
-                                <input type="password" class="form-control" id="cvCode" placeholder="CV" required />
-                            </div> <!-- .form-group -->
-                        </div> <!-- .col-xs-5 .col-md-5 .pull-right -->
-                    </div> <!-- .row -->
-                    </form>
-                </div> <!-- .panel-body -->            
+            <div class="panel-body">
+			  <form role="form">
+				<div class="form-group">
+				  <label for="cardNumber">Card Number</label>
+				  <div class="input-group">
+					<input type="text" class="form-control" id="cardNumber" placeholder="Valid Card Number" required autofocus />
+					<span class="input-group-addon">
+					  <span class="glyphicon glyphicon-lock"></span>
+					</span>
+				  </div> <!-- .input-group -->
+				</div> <!-- .form-group -->
+			    <div class="row">
+				  <div class="col-xs-7 col-md-7">
+					<div class="form-group">
+					  <label for="expiryMonth">Card Expiry Date</label>
+					  <div class="col-xs-6 col-lg-6 pl-ziro">
+						<input type="text" class="form-control" id="expityMonth" placeholder="MM" required />
+					  </div> <!-- .col-xs-6 .col-lg-6 .pl-ziro -->
+					  <div class="col-xs-6 col-lg-6 pl-ziro">
+						<input type="text" class="form-control" id="expityYear" placeholder="YY" required />
+					  </div> <!-- .col-xs-6 .col-lg-6 .pl-ziro -->
+					</div> <!-- .form-group -->
+				  </div> <!-- .col-xs-7 .col-md-7 -->
+			      <div class="col-xs-5 col-md-5 pull-right">
+					<div class="form-group">
+					  <label for="cvCode">CV Code</label>
+					  <input type="password" class="form-control" id="cvCode" placeholder="CV" required />
+					</div> <!-- .form-group -->
+			      </div> <!-- .col-xs-5 .col-md-5 .pull-right -->
+			    </div> <!-- .row -->
+			  </form>
+            </div> <!-- .panel-body -->            
             
+            <ul class="nav nav-pills nav-stacked">
+              <li class="active">
+                <a href="#">
+                  <span class="badge pull-right">
+                    <span class="glyphicon glyphicon-usd">
+                    </span>XXX.XX
+                  </span> Final Payment</a>
+              </li>
+            </ul>
+            <br />
+            <a href="#" class="btn btn-success btn-lg btn-block" role="button">Pay</a>
             
         </div> <!-- .col-xs-12 .col-md-4 end -->
     </div> <!-- .row end -->
