e41ce18ac70ef4c75e168964cd22ad0fe26e4c88
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-10-07 14:58:06 -0500

    Created form

--- checkout.php
+++ checkout.php
@@ -23,5 +23,53 @@
     <![endif]-->
 </head>
 <body>
+
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-md-4">
+            <div class="panel-heading">
+              <h3 class="panel-title">Payment Details</h3>
+            </div>	<!-- .panel-heading -->   
+            
+                <div class="panel-body">
+                    <form role="form">
+                      <div class="form-group">
+                        <label for="cardNumber">Card Number</label>
+                          <div class="input-group">
+                            <input type="text" class="form-control" id="cardNumber" placeholder="Valid Card Number"
+                                required autofocus />
+                            <span class="input-group-addon">
+                              <span class="glyphicon glyphicon-lock"></span>
+                            </span>
+                          </div>    <!-- .input-group -->
+                      </div>	<!-- .form-group -->
+                    <div class="row">
+                        <div class="col-xs-7 col-md-7">
+                            <div class="form-group">
+                                <label for="expiryMonth">
+                                    Card Expiry Date</label>
+                                <div class="col-xs-6 col-lg-6 pl-ziro">
+                                    <input type="text" class="form-control" id="expityMonth" placeholder="MM" required />
+                                </div>
+                                <div class="col-xs-6 col-lg-6 pl-ziro">
+                                    <input type="text" class="form-control" id="expityYear" placeholder="YY" required /></div>
+                            </div>
+                        </div>
+                        <div class="col-xs-5 col-md-5 pull-right">
+                            <div class="form-group">
+                                <label for="cvCode">
+                                    CV Code</label>
+                                <input type="password" class="form-control" id="cvCode" placeholder="CV" required />
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            
+        </div> <!-- .col-xs-12 .col-md-4 end -->
+    </div> <!-- .row end -->
+  </div>	<!-- .container end -->
+
+
 </body>
 </html>
